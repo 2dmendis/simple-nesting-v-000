4fc8fa1a8132bba4305751194b298c33a5aee82b
Dmitri Mendis <2dmendis@gmail.com>
2018-05-09 23:27:06 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     new = programmer_hash[:dennis_ritchie][:languages]
+     newer = new.join
+     newer
 end
 
 def adding_matz
@@ -79,6 +84,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     #programmer_hash[:yukihiro_matsumoto]
+     #programmer_hash = :yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}
+     programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+     #programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+     #programmer_hash[:yukihiro_matsumoto][:languages] = ["Lisp", "C"]
+     
 
     
 end
@@ -101,7 +112,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
 end
 
